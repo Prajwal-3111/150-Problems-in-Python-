dc045f71aaaa284e75d3064adfd6b06b9aee07e5
Prajwal-3111 <prajwalr675@gmail.com>
2026-08-18 20:24:45 +0530

    Add files via upload

---
+++ Problem2.py
@@ -0,0 +1,4 @@
+#Ask for the user’s first name and then ask for their surname and display the output message Hello [First Name] [Surname]. 
+name = input("Please enter your first name: ")
+surname = input("Please enter your surname: ")
+print("Hello " + name + " " + surname + ".")
